e72fdf60c69d93318253cea147f75c4bf7ea1a4e
Matt Wright <mattwright@squareup.com>
2013-01-16 12:08:03 -0800

    Use environment variables for Omniauth config Previously only did so for Twitter gem config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'Xf4chFu45f7HBTFpVqmeQ', '34xT21j3Ba1xvZ7AOCUnHepw06N6FjZXfjsNp4vjg'
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
