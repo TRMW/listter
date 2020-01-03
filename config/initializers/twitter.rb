7f9f2aa1045758c4b412f3712a616a065a489d0c
Matt Wright <mattwright@squareup.com>
2020-01-02 16:58:19 -0800

    Add Twitter access token and secret config vars

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,6 @@
 Twitter.configure do |config|
   config.consumer_key = ENV['TWITTER_KEY']
   config.consumer_secret = ENV['TWITTER_SECRET']
+  config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
